9a7dcf09c3cd78b390c4d2e9116022c994c8befe
Marc Charmois <marc.charmois@hotmail.com>
2019-10-07 18:47:48 +0200

    adding sendgrid

--- eventhub-trigger/__init__.py
+++ eventhub-trigger/__init__.py
@@ -6,3 +6,6 @@ import azure.functions as func
 def main(event: func.EventHubEvent):
     logging.info('Python EventHub trigger processed an event: %s',
                  event.get_body().decode('utf-8'))
+    eventBody = event.get_body().decode('utf-8')
+    if("test" in eventBody):
+        print("custom event found")
